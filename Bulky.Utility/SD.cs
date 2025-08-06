54458ca5a2bed1dc7c96c453d3aea613439a0dc5
Shraddha Jadhav <shraddhaj478@gmail.com>
2025-08-06 16:42:37 +0530

    Add files via upload

---
+++ Bulky.Utility/SD.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Utility
+{
+    public static class SD
+    {
+        public const string Role_Customer = "Customer";
+        public const string Role_Company = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusProcessing = "Processing";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusProcessing = "Processing";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+        public const string SessionCart = "SessionShoppingCart";
+        public static string? StatusInprocess;
+        public static string? statusShipped;
+
+        public static string? PaymentStatusPending { get; set; }
+        public static string? PaymentStatusDelayPending { get; set; }
+    }
+}
